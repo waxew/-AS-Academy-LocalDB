c513a1fef8dda0a1d9f83d17695a5a5d074caae5
waxew <4622.ar@gmail.com>
2026-08-30 04:11:55 +0330

    Include AS Academy engine module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,5 +26,7 @@ include(":academy-course")
 // ماژول‌های مشترک مستقیماً از Core مرکزی استفاده می‌شوند.
 include(":course")
 project(":course").projectDir = file("academy-core/course")
+include(":engine")
+project(":engine").projectDir = file("academy-core/engine")
 include(":core")
 project(":core").projectDir = file("academy-core/core")
